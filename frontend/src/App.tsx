2b406d599dfa80a5891ded4f3afe72ecab09fb17
ReeseHatfield <hatfield.69@wright.edu>
2024-09-20 15:24:35 -0400

    fix router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,18 +9,19 @@ import Home from './pages/home/Home'
 import Game from './pages/game/Game'
 
 function App() {
-    const [count, setCount] = useState(0)
+
 
     return (
         <>
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home />}> </Route>
+                    <Route path='/home' element={<Home />}> </Route>
                     <Route path='/game' element={<Game />}> </Route>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </>
     )
+
 }
 
 export default App
