8dae7c894a305c2afa092d13bc60235a9fd29517
shepard <shepard>
2024-10-08 12:44:35 -0400

    Fixing tmux sessions and panels

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-cd frontend
-npm i
-npm run dev &
-cd ..
-cd backend
-npm i
-npm run dev
+if which tmux >/dev/null; then
+	echo "You are running in tmux"
+	tmux new-session -d -s geo
+
+	tmux send-keys -t geo "cd backend" ENTER
+	tmux send-keys -t geo "npm i" ENTER
+	tmux send-keys -t geo "npm run dev" ENTER
+
+	tmux split-window -h -t geo
+	tmux send-keys -t geo:0 "cd frontend" ENTER
+	tmux send-keys -t geo:0 "npm i" ENTER
+	tmux send-keys -t geo:0 "npm run dev" ENTER
+
+	tmux attach-session -t geo
+else
+	echo "Running normally"
+	cd backend
+	npm i
+	npm run dev &
+	cd ../frontend
+	npm i
+	npm run dev
+fi
