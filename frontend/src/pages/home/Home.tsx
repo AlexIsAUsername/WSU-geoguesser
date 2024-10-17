4dbb2f1324eb25ce409684ec7a1e8e89230b2a06
ReeseHatfield <hatfield.69@wright.edu>
2024-10-17 15:04:40 -0400

    better styles

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,23 +1,21 @@
 import React, { FormEvent, useState } from 'react'
-import { NavButton } from '../../components/NavButton/NavButton'
 import { useNavigate } from 'react-router-dom';
+import "./Home.css";
 
 const Home = () => {
 
     const [key, setKey] = useState<string>("");
-    const nav = useNavigate()
+    const nav = useNavigate();
 
-    
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-        if(key != ""){
-            setKeyOnFS(key)
+        if (key !== "") {
+            setKeyOnFS(key);
         }
-        nav("/game")
+        nav("/game");
     }
 
-    
     const setKeyOnFS = async (apiKey: string) => {
         const res = await fetch("http://localhost:4000/setkey", {
             method: "POST",
@@ -27,34 +25,31 @@ const Home = () => {
             body: JSON.stringify({
                 key: apiKey
             }) 
-        })
+        });
 
         const data: string = await res.json();
 
-        if (res.status != 200){
-            console.log("sum wrong :(");
+        if (res.status !== 200) {
+            console.log("Something went wrong :(");
             console.log(data);
         }
-
     }
 
-
     return (
-        <>
-            <div>Welcome to WSU Geoguesser</div>
+        <div className="home-container">
+            <div className="home-title">Welcome to WSU Geoguesser</div>
 
-            <form onSubmit={handleSubmit}>
-
-                <input 
+            <form className="home-form" onSubmit={handleSubmit}>
+                <input
                     type="text"
+                    className="home-input"
+                    placeholder="Enter API Key"
                     onChange={(e) => setKey(e.target.value)}
                 />
-
-                <button type='submit'>Submit</button>
-
+                <button type='submit' className="home-button">Submit</button>
             </form>
-        </>
+        </div>
     )
 }
-export default Home
 
+export default Home;
