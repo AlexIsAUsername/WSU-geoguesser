6d46fbac18439b3eced935bc81b8f3cfb512df5f
ReeseHatfield <hatfield.69@wright.edu>
2024-10-17 14:40:14 -0400

    add top lvl default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/home/Home'
 import Game from './pages/game/Game'
@@ -17,6 +13,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Home />}> </Route>
+                    <Route path='/home' element={<Home />}> </Route>
                     <Route path='/game' element={<Game />}> </Route>
                     <Route path='/score' element={<Score />}> </Route>
                 </Routes>
